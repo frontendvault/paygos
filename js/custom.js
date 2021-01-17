9e76315a75924eb045eb74cb45d528324b543ee2
jimmy07 <manoj07.pathania@gmail.com>
2021-01-18 04:42:59 +0530

    arrow carousel

--- js/custom.js
+++ js/custom.js
@@ -21,7 +21,7 @@
               slidesToScroll: 1
           }
       }, ],
-      prevArrow: "<img class='a-left control-c prev slick-prev' src='../images/kitkat.png'>",
+      prevArrow: "<img class='a-left control-c prev slick-prev' src='images/kitkat.png'>",
       nextArrow: "<img class='a-right control-c next slick-next' src='../images/arrow-right.png'>"
   });
 
