a3e7cab3bf42fc589be856f41542339ff697c784
jimmy07 <manoj07.pathania@gmail.com>
2021-01-18 04:46:20 +0530

    carousel arrow left

--- js/custom.js
+++ js/custom.js
@@ -21,7 +21,7 @@
               slidesToScroll: 1
           }
       }, ],
-      prevArrow: "<img class='a-left control-c prev slick-prev' src='images/arrow-right.png'>",
+      prevArrow: "<img class='a-left control-c prev slick-prev' src='images/arrow-left.png'>",
       nextArrow: "<img class='a-right control-c next slick-next' src='images/arrow-right.png'>"
   });
 
